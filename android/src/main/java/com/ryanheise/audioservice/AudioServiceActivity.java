6ea57e73ffc89b7c441de060d6044509a1b62135
harpreetslabs <harpreet@sookshum-labs.com>
2024-12-17 10:45:14 +0530

    initial commit

---
+++ android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -0,0 +1,16 @@
+package com.ryanheise.audioservice;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class AudioServiceActivity extends FlutterActivity {
+    @Override
+    public FlutterEngine provideFlutterEngine(@NonNull Context context) {
+        AudioServicePlugin.setArmStartup(true);
+        return AudioServicePlugin.getFlutterEngine(context);
+    }
+}
